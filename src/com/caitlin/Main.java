f63e79add1a11f8ee23eb2bfdc01f59134d65eef
Caitlin Swickard <caitlin6029@gmail.com>
2022-05-10 12:12:10 -0600

    created command line application to print hello world

---
+++ src/com/caitlin/Main.java
@@ -0,0 +1,13 @@
+//package statement
+package com.caitlin;
+
+//public access, class Main
+public class Main {
+//    public access method, static(the main method in your program should always be static), returnType void,
+//    main method, one param in the ()
+    public static void main(String[] args) {
+//        write your code here
+        System.out.println("Hello World, my name is Caitlin");
+
+    }
+}
