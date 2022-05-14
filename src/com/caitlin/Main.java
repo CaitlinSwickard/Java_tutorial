0567615dbe5da517bfb98b1c433d141be341279c
Caitlin Swickard <caitlin6029@gmail.com>
2022-05-14 12:59:00 -0600

    fizzbuzz exercise

--- src/com/caitlin/Main.java
+++ src/com/caitlin/Main.java
@@ -9,14 +9,15 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-//constants
+//constants to avoid magic nums
         final byte MONTHS_IN_YEAR = 12;
         final byte PERCENT = 100;
 
+//reading input (scanner)
         Scanner scanner = new Scanner(System.in);
 //asking our first question, removing ln to type inline with question
         System.out.print("What is your Principal: ");
-        int principal = scanner.nextInt();
+           int principal = scanner.nextInt();
 
         System.out.print("What is your Annual Interest Rate: ");
         float annualInterest = scanner.nextFloat();
@@ -30,7 +31,30 @@ public class Main {
                 * (monthlyInterest * Math.pow(1 + monthlyInterest, numberOfPayments))
                 / (Math.pow(1 + monthlyInterest, numberOfPayments) -1);
 
+        //format this value in a currency
         String mortgageFormatted = NumberFormat.getCurrencyInstance().format(mortgage);
         System.out.println("Mortgage: " + mortgageFormatted);
     }
 }
+
+
+//fizz buzz exercise
+//import java.util.Scanner;
+
+//public class Main {
+//    public static void main(String[] args) {
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.print("Number: ");
+//        int number = scanner.nextInt();
+//
+//        if (number % 5 == 0 && number % 3 == 0)
+//            System.out.println("FizzBuzz");
+//        else if (number % 5 == 0)
+//            System.out.println("Fizz");
+//        else if (number % 3 == 0);
+//            System.out.println("Buzz");
+//            else
+//                System.out.println(number);
+//    }
+//}
+
